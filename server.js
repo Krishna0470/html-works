640fa2632fc0c41306509191d849b6aa7818e88f
krishna <krishnasabu885@gmail.com>
2024-01-16 11:34:01 +0530

    hello

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const port =5000;
+
+console.log("__dirname :", __dirname);
+app.use('/',express.static(__dirname + '/client'));
+
+
+app.get('/test',(req,res)=>{
+    res.status(200).send("Success");
+
+});
+
+
+app.listen(port,()=>{
+    console.log(`server running at http://localhost:${port}`)
+});
