0e440affb4c8028435a35fc7621b850907a3e3c2
krishna <krishnasabu885@gmail.com>
2024-02-14 11:31:11 +0530

    hello

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,15 @@
+import { Fragment } from "react";
+
+
+
+function App() {
+
+  return (
+    <Fragment>
+    <h1>hello React</h1>
+    <h2>Hello</h2>
+    </Fragment>
+  )
+}
+
+export default App;
