126828bbc1142760f145ba4c57894c70f77f07a5
krishna <krishnasabu885@gmail.com>
2024-02-21 11:16:41 +0530

    hello

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,43 @@
+
+
+// import Counter from "./components/Counter";
+// import Counter1 from "./components/Counter1";
+// import Parent from "./components/forms/Parent";
+// import Login from "./components/conditional_rendering/Login";
+// import Controlled from "./components/controlledAndUncontrolled/Controlled";
+// import Uncontrolled from "./components/controlledAndUncontrolled/Uncontrolled";
+// import MyComponentWithUpperCase from "./components/higherOrderComponents/MyComponentWithUpperCase";
+// import ParentComponent from "./components/pure_components/ParentComponent";
+// import MyComponent from "./components/pure_components/MyComponent";
+// import BrowserRouter from "./components/routing/BasicRouting/BasicRouting";
+// import NestedRouting from "./components/routing/NestedRouting/NestedRouting";
+import UseCallbackComponent from "./components/hooks/UseCallbackComponent";
+
+
+function App() {
+
+  return (
+    <>
+      {/* <Counter initialCount ={12} />
+   <Counter1 initialCount={10} /> */}
+
+
+  {/* <Login/> */}
+
+  {/* <Controlled/> */}
+  {/* <Uncontrolled/> */}
+
+  {/* <MyComponentWithUpperCase text={"hello"} /> */}
+  {/* <MyComponent name="hellooo"/> */}
+  {/* <ParentComponent/> */}
+  {/* <BrowserRouter/> */}
+
+  {/* <NestedRouting/> */}
+
+  <UseCallbackComponent/>
+    </>
+  
+  )
+}
+
+export default App;
